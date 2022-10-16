81ea2ee0fe51e0d23046d2bcecb4508afc15e89b
Hemanth Saju <107930234+hemanthsaju7@users.noreply.github.com>
2022-10-16 11:49:15 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "cidr_block" {
+  default = "172.25.0.0/16"
+}
+
+variable "project_name" {
+  default = "zomato"
+}
+
+variable "project_env" {
+  default = "prod"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "hosted_zone" {
+  default = "Z05650136NGTW3V3ENDY"
+}
